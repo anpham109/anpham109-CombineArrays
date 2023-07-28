a8da898e80f7f754a2f0433070f14e21f1836a64
Andy Pham <anpham-work@protonmail.com>
2023-07-28 15:33:31 +0000

    finished the challenge

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,13 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] newArray = new int[arr1.length + arr2.length];
+        for(int i = 0; i < arr1.length; i++){
+            newArray[i] = arr1[i];
+        }
+        for(int i = arr1.length; i < newArray.length; i++){
+            newArray[i] = arr2[i - arr1.length];
+        }
+        return newArray;
     }
 }
